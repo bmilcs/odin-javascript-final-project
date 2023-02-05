22fa75b0d562196e10783f58b9c7b3bfa668f228
Bryan Miller <bmilcs@yahoo.com>
2023-02-05 18:03:17 -0500

    Add base URL to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/odin-javascript-final-project/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
