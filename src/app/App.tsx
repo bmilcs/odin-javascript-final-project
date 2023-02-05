4888a646b02f666b49f6de25f862b747460dcb49
Bryan Miller <bmilcs@yahoo.com>
2023-02-04 19:17:58 -0500

    Test api key loading from .env

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,9 @@
 function App() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
   return (
     <div className="App">
       <h1>headliners</h1>
+      <p>api key via .env test: {API_KEY}</p>
     </div>
   );
 }
