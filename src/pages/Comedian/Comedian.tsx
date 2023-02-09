0f1cad1b2a4e6ad8e0405123ff6a1e9c0ed622da
Bryan Miller <bmilcs@yahoo.com>
2023-02-09 17:57:16 -0500

    Add build/ updates

---
+++ src/pages/Comedian/Comedian.tsx
@@ -0,0 +1,71 @@
+import { getAllSpecialsForPersonURL, getPersonDetailsURL } from "@/api/TMDB";
+import useFetch from "@/hooks/useFetch";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./Comedian.scss";
+
+function Comedian() {
+  const { personId } = useParams();
+  const id = personId ? Number(personId) : 0;
+  const comedianURL = getPersonDetailsURL(id);
+  const specialsURL = getAllSpecialsForPersonURL(id);
+  const {
+    data: personalData,
+    error: personalError,
+    isLoading: personalIsLoading,
+  } = useFetch(comedianURL);
+  const {
+    data: specialsData,
+    error: specialsError,
+    isLoading: specialsIsLoading,
+  } = useFetch(specialsURL);
+
+  return (
+    <>
+      <div className="person">
+        {personalData && (
+          <>
+            <p>{personalData.name}</p>
+            <p>{personalData.id}</p>
+            <p>{personalData.birthday}</p>
+            <p>{personalData.biography}</p>
+            <p>{personalData.imdb_id}</p>
+            <img
+              className="headshot"
+              src={`https://image.tmdb.org/t/p/original/${personalData.profile_path}`}
+              alt=""
+            ></img>
+          </>
+        )}
+      </div>
+
+      <div className="specials">
+        {specialsData && (
+          <>
+            {specialsData.results.map((special: any) => {
+              return (
+                <div>
+                  <h2>.title: {special.title}</h2>
+                  <p>.id: {special.id}</p>
+                  <p>.release_date: {special.release_date}</p>
+                  <p>.vote_average: {special.vote_average}</p>
+                  <p>.overview: {special.overview}</p>
+                  .backdrop_path:
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${special.backdrop_path}`}
+                  />
+                  {/* .poster_path:
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${special.poster_path}`}
+                  /> */}
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Comedian;
