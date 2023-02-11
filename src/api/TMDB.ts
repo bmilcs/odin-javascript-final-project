d4c541bc6ed4758aa15d48175b6a0ec757a82973
Bryan Miller <bmilcs@yahoo.com>
2023-02-11 16:17:01 -0500

    Add interface for tmdb movie api

--- src/api/TMDB.ts
+++ src/api/TMDB.ts
@@ -139,19 +139,60 @@ export const searchAllURL = (name: string): string => {
 // typescript types
 //
 
-export interface TDiscoverMovieResult {
-  poster_path?: string | null;
+export interface IDiscoverMovieResult {
   adult?: boolean;
-  overview?: string;
-  release_date?: string;
+  backdrop_path?: string | null;
   genre_ids?: number[];
   id?: number;
-  original_title?: string;
   original_language?: string;
-  title?: string;
-  backdrop_path?: string | null;
+  original_title?: string;
+  overview?: string;
   popularity?: number;
-  vote_count?: number;
+  poster_path?: string | null;
+  release_date?: string;
+  title?: string;
   video?: boolean;
   vote_average?: number;
+  vote_count?: number;
+}
+
+export interface IMovieDetails {
+  adult?: boolean;
+  backdrop_path?: string | null;
+  belongs_to_collection?: null | object;
+  budget?: number;
+  genre_ids?: {
+    id?: number;
+    name?: string;
+  }[];
+  id?: number;
+  imdb_id?: string | null;
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string | null;
+  production_companies: {
+    name?: string;
+    id?: number;
+    logo_path?: string | null;
+    origin_country?: string;
+  }[];
+  production_countries: { iso_3166_1?: string; name?: string }[];
+  release_date?: string;
+  revenue?: number;
+  runtime?: number | null;
+  spoken_languages: { iso_639_1?: string; name?: string }[];
+  status:
+    | "Rumored"
+    | "Planned"
+    | "In Production"
+    | "Post Production"
+    | "Released"
+    | "Cancelled";
+  tagline?: string | null;
+  title?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
 }
