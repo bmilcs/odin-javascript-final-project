6723537f3e7ea707366c325acd2f3680c0808833
Bryan Miller <bmilcs@yahoo.com>
2023-02-11 16:20:49 -0500

    Add Special pages with routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Comedian from "@/pages/Comedian/Comedian";
 import Home from "@/pages/Home/Home";
+import Special from "@/pages/Special/Special";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path={`/comedians/:personId`} element={<Comedian />} />
+          <Route path={`/specials/:specialId`} element={<Special />} />
         </Routes>
       </main>
 
---
+++ src/pages/Special/Special.tsx
@@ -0,0 +1,94 @@
+import {
+  getTMDBImageURL,
+  getIMDBURL,
+  IDiscoverMovieResult,
+  getMovieDetailsURL,
+} from "@/api/TMDB";
+import SpecialCard from "@/components/SpecialCard/SpecialCard";
+import useFetch from "@/hooks/useFetch";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./Special.scss";
+
+// TODO validate incoming api ID
+
+function Comedian() {
+  const { specialId } = useParams();
+  const id = specialId ? Number(specialId) : 0;
+  const specialURL = getMovieDetailsURL(id);
+  // const comedianURL = getPersonDetailsURL(id);
+  // const {
+  //   data: personalData,
+  //   error: personalError,
+  //   isLoading: personalIsLoading,
+  // } = useFetch(comedianURL);
+  const {
+    data: specialData,
+    error: specialError,
+    isLoading: specialIsLoading,
+  } = useFetch(specialURL);
+
+  useEffect(() => {
+    console.log("specialdata: ", specialData);
+  }, [specialData]);
+
+  return (
+    <div className="column">
+      {specialData && (
+        <div className="special">
+          <div>
+            {/* image */}
+            {specialData.backdrop_path ? (
+              <img
+                className="special__image"
+                src={getTMDBImageURL(specialData.backdrop_path)}
+                alt={`${specialData.title}`}
+              />
+            ) : specialData.poster_path ? (
+              <img
+                className="special__image"
+                src={getTMDBImageURL(specialData.poster_path)}
+                alt={`${specialData.title}`}
+              />
+            ) : (
+              ""
+            )}
+
+            {/* information */}
+            <div className="special__content">
+              {specialData.title && (
+                <h2 className="special__title">{specialData.title}</h2>
+              )}
+              {specialData.homepage && (
+                <p className="special__homepage">{specialData.homepage}</p>
+              )}
+              {specialData.release_date && (
+                <p className="special__release_date">
+                  {specialData.release_date}
+                </p>
+              )}
+              {specialData.vote_average && (
+                <p className="special__vote_average">
+                  {specialData.vote_average}/10
+                </p>
+              )}
+              {specialData.runtime && (
+                <p className="special__runtime">
+                  {specialData.runtime} minutes
+                </p>
+              )}
+              {specialData.status && specialData.status !== "Released" && (
+                <p className="special__status">{specialData.status}</p>
+              )}
+              {specialData.overview && (
+                <p className="special__overview">{specialData.overview}</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Comedian;
