09663df92aed24bb06d4f155946447efe7ca425d
Bryan Miller <bmilcs@yahoo.com>
2023-02-19 18:02:40 -0500

    Add search results & add comedian modal component

---
+++ src/components/AddComedianModal/AddComedianModal.tsx
@@ -0,0 +1,90 @@
+import {
+  IDiscoverMovieResult,
+  IPersonDetailsResult,
+  getTMDBImageURL,
+} from "@/api/TMDB";
+import MicrophoneSVG from "@/assets/MicrophoneSVG";
+import Button from "@/components/Button/Button";
+import SpecialsList from "@/components/SpecialsList/SpecialsList";
+import useFetchPersonalAndSpecialsData from "@/hooks/useFetchPersonalAndSpecialsData";
+import "./AddComedianModal.scss";
+
+function AddComedianModal({
+  personId,
+  handleAddComedian,
+}: {
+  personId: number;
+  handleAddComedian: (
+    personalData: IPersonDetailsResult,
+    specials: IDiscoverMovieResult,
+    appearances: IDiscoverMovieResult
+  ) => void;
+}) {
+  const { specials, appearances, personalData } =
+    useFetchPersonalAndSpecialsData(personId);
+
+  return (
+    <div className="modal" onClick={(e) => e.stopPropagation()}>
+      {personalData && (
+        <div className="modal__personal">
+          {/* <Link to={`/comedians/${personalData.id}`}> */}
+          {personalData.profile_path ? (
+            <img
+              src={getTMDBImageURL(personalData.profile_path)}
+              alt={`${personalData.name} Headshot`}
+              className="modal__image"
+            />
+          ) : (
+            <MicrophoneSVG className="modal__image modal__svg" />
+          )}
+          {/* </Link> */}
+
+          <h4 className="modal__name">{personalData.name}</h4>
+        </div>
+      )}
+
+      {specials &&
+      appearances &&
+      personalData &&
+      personalData.name &&
+      (appearances.length !== 0 || specials.length !== 0) ? (
+        // person has a special or appearance
+        <>
+          {specials.length > 0 && (
+            <>
+              <h3>Specials</h3>
+              <SpecialsList data={specials} />
+            </>
+          )}
+          {appearances.length > 0 && (
+            <>
+              <h3>Appearances</h3>
+              <SpecialsList data={appearances} />
+            </>
+          )}
+
+          <Button onClick={() => handleAddComedian(personalData.id)}>
+            Add Comedian
+          </Button>
+
+          <p className="modal__warning">
+            If the person above is not a standup comedian,{" "}
+            <span className="modal__warning__span">
+              please do not add them to the site.
+            </span>
+          </p>
+        </>
+      ) : (
+        // person isn't a comedian
+        <div className="modal__not-comedian">
+          <p className="modal__not-comedian__error">
+            No standup specials found.
+          </p>
+          <p>Sorry. We're unable to add this person to the site.</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AddComedianModal;
--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -1,20 +1,26 @@
 import {
   getTMDBImageURL,
+  IDiscoverMovieResult,
+  IPersonDetailsResult,
   IPersonSearchResult,
   parseSearchQuery,
   searchForPersonURL,
 } from "@/api/TMDB";
 import MicrophoneSVG from "@/assets/MicrophoneSVG";
+import AddComedianModal from "@/components/AddComedianModal/AddComedianModal";
 import useFetch from "@/hooks/useFetch";
-import { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./SearchResults.scss";
+import { addComedianToDB } from "@/firebase/database";
 
 function SearchResults() {
   const { searchTerm } = useParams();
   const term = parseSearchQuery(searchTerm!);
   const url = searchForPersonURL(term);
   const { data, isLoading, setUrl } = useFetch(url);
+  const [showModal, setShowModal] = useState(false);
+  const [modalPersonId, setModalPersonId] = useState<number>();
 
   // update the results if the search term is changed while on the page
   useEffect(() => {
@@ -22,80 +28,73 @@ function SearchResults() {
     setUrl(newUrl);
   }, [term]);
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  const handleAddComedian = (personalId: number) => {
+    addComedianToDB(personalId);
+  };
+
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
 
   return (
     <div className="column">
       <div className="searchpage">
         <div className="searchpage__header">
-          <div className="left-column">
-            <h2 className="searchpage__title">Comedian Search</h2>
-            <p className="searchpage__details">
-              For: <span className="searchpage__term">" {term} "</span>
-            </p>
-          </div>
-          <div className="right-column">
-            <p>
-              To add a comedian to the site, click on the person's image and
-              click on the "Add Comedian" button.
-            </p>
-          </div>
+          <h2 className="searchpage__title">Comedian Search</h2>
+          <p className="searchpage__details">
+            You searched for:{" "}
+            <span className="searchpage__term">" {term} "</span>
+          </p>
+          {/* <p className="searchpage__instructions">
+            To add a comedian to the site, click on the person's image and click
+            on the "Add Comedian" button.
+          </p> */}
         </div>
-
-        {data && data.results && data.length !== 0 && (
-          // when results are present:
-          <div className="searchpage__grid">
-            {data.results
-              // sort by popularity
-              .sort((a: IPersonSearchResult, b: IPersonSearchResult) => {
-                return a.popularity > b.popularity ? -1 : 1;
-              })
-              // create clickable cards each person
-              .map((person: IPersonSearchResult) => (
-                <div className="searchpage__person" key={person.id}>
-                  <Link to={`/comedians/${person.id}`}>
-                    {person.profile_path ? (
-                      <img
-                        src={getTMDBImageURL(person.profile_path)}
-                        alt={`${person.name} Headshot`}
-                        className="searchpage__headshot"
-                      />
-                    ) : (
-                      <MicrophoneSVG className="comedian-card__image comedian-card__svg" />
-                    )}
-                    <h3 className="searchpage__name">{person.name}</h3>
-                  </Link>
-                </div>
-              ))}
-          </div>
-        )}
       </div>
+
+      {data && data.results && data.length !== 0 && (
+        // when results are present:
+        <div className="searchpage__grid">
+          {data.results
+            // sort by popularity
+            .sort((a: IPersonSearchResult, b: IPersonSearchResult) => {
+              return a.popularity > b.popularity ? -1 : 1;
+            })
+            // create clickable cards each person
+            .map((person: IPersonSearchResult) => (
+              <div
+                className="searchpage__person"
+                key={person.id}
+                onClick={() => {
+                  setShowModal(true);
+                  setModalPersonId(person.id);
+                }}
+              >
+                {person.profile_path ? (
+                  <img
+                    src={getTMDBImageURL(person.profile_path)}
+                    alt={`${person.name} Headshot`}
+                    className="searchpage__headshot"
+                  />
+                ) : (
+                  <MicrophoneSVG className="comedian-card__image comedian-card__svg" />
+                )}
+                <h3 className="searchpage__name">{person.name}</h3>
+              </div>
+            ))}
+        </div>
+      )}
+
+      {showModal && modalPersonId && (
+        <div className="overlay" onClick={(e) => setShowModal(false)}>
+          <AddComedianModal
+            personId={modalPersonId}
+            handleAddComedian={handleAddComedian}
+          />
+        </div>
+      )}
     </div>
   );
 }
 
 export default SearchResults;
-
-// return (
-//   <div
-//     className="searchpage__result"
-//     data-tmdb={person.id}
-//     key={person.id}
-//   >
-//     {person.profile_path ? (
-//       <img
-//         src={getTMDBImageURL(person.profile_path)}
-//         alt={`${person.name} Headshot`}
-//         className="searchpage__photo"
-//       />
-//     ) : (
-//       <MicrophoneSVG className="searchpage__photo searchpage__svg" />
-//     )}
-//     {person.name && (
-//       <p className="searchpage__name">{person.name}</p>
-//     )}
-//   </div>
-// );
-// })
