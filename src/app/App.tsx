0db7ca7997ffd40f3433338bb2dd2964bdc9511c
Bryan Miller <bmilcs@yahoo.com>
2023-02-20 17:06:40 -0500

    Remove getAllSpecials on initial page load

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import { getAllSpecialsFromDB } from "@/firebase/database";
 import Comedian from "@/pages/Comedian/Comedian";
 import Home from "@/pages/Home/Home";
 import SearchResults from "@/pages/SearchResults/SearchResults";
@@ -9,8 +8,6 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  getAllSpecialsFromDB();
-
   return (
     <div className="app">
       <Header />
