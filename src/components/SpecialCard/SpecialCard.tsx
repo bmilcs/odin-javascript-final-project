cb70794e0d7862e7556bf7d1136d94c6c9fe1a63
Bryan Miller <bmilcs@yahoo.com>
2023-02-26 16:32:28 -0500

    Refactor special card to use prefetched data

--- src/components/SpecialCard/SpecialCard.tsx
+++ src/components/SpecialCard/SpecialCard.tsx
@@ -11,67 +11,54 @@ import MicrophoneSVG from "@/assets/MicrophoneSVG";
 import FavoriteIcon from "@/components/FavoriteIcon/FavoriteIcon";
 import Card from "@/components/Card/Card";
 import "./SpecialCard.scss";
+import { IComedySpecial } from "@/firebase/database";
 
-function SpecialCard({ id }: IDiscoverMovieResult) {
-  const specialURL = getMovieDetailsURL(id);
-  const { data, error } = useFetch(specialURL);
-  const [special, setSpecial] = useState<IDiscoverMovieResult | undefined>();
-
-  useEffect(() => {
-    if (data && data.title) {
-      const { title, ...rest } = data;
-      const card_title = title.includes(": ") ? title.split(": ")[1] : title;
-      setSpecial({ card_title, title, ...rest });
-    }
-  }, [data]);
-
-  // useEffect(() => {
-  //   console.log("special", special);
-  // }, [special]);
-
+function SpecialCard({
+  id,
+  title,
+  backdrop_path,
+  poster_path,
+  release_date,
+}: IComedySpecial) {
   return (
     <>
-      {special && (
-        <Card className="special-card" dataAttribute={`special-${special.id}`}>
-          <>
-            {/* image */}
-            <Link to={`/specials/${special.id}`}>
-              {special.backdrop_path ? (
-                <img
-                  className="special-card__image"
-                  src={getTMDBImageURL(special.backdrop_path)}
-                  alt={`${special.title}`}
-                />
-              ) : special.poster_path ? (
-                <img
-                  className="special-card__image"
-                  src={getTMDBImageURL(special.poster_path)}
-                  alt={`${special.title}`}
-                />
-              ) : (
-                <MicrophoneSVG className="special-card__image special-card__svg" />
-              )}
-              {special.release_date && (
-                <p className="special-card__year">
-                  {formatDateYearOnly(special.release_date)}
-                </p>
-              )}
-            </Link>
+      <Card className="special-card" dataAttribute={`special-${id}`}>
+        <>
+          {/* image */}
+          <Link to={`/specials/${id}`}>
+            {backdrop_path ? (
+              <img
+                className="special-card__image"
+                src={getTMDBImageURL(backdrop_path)}
+                alt={`${title}`}
+              />
+            ) : poster_path ? (
+              <img
+                className="special-card__image"
+                src={getTMDBImageURL(poster_path)}
+                alt={`${title}`}
+              />
+            ) : (
+              <MicrophoneSVG className="special-card__image special-card__svg" />
+            )}
+            {release_date && (
+              <p className="special-card__year">
+                {formatDateYearOnly(release_date)}
+              </p>
+            )}
+          </Link>
 
-            {/* text details */}
-            <div className="special-card__content">
-              {special.title && (
-                <p className="special-card__title">{special.card_title}</p>
-              )}
+          {/* text details */}
+          <div className="special-card__content">
+            {title && <p className="special-card__title">{title}</p>}
 
-              <FavoriteIcon favoriteId={`special-${special.id}`} />
-              {/* {special.vote_average && (
+            <FavoriteIcon favoriteId={`special-${id}`} />
+            {/* {special.vote_average && (
               <p className="special-card__vote">{special.vote_average}</p>
             )} */}
-            </div>
-          </>
-        </Card>
-      )}
+          </div>
+        </>
+      </Card>
     </>
   );
 }
