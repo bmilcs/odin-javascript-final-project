b40d2b73fbea0cc067485f92b0c5b69419af3233
Bryan Miller <bmilcs@yahoo.com>
2023-02-26 16:28:05 -0500

    Add comedians grid component

---
+++ src/components/ComedianGrid/ComedianGrid.tsx
@@ -0,0 +1,25 @@
+import ComedianCard from "@/components/ComedianCard/ComedianCard";
+import "./ComedianGrid.scss";
+import { IComedian, IComedySpecial } from "@/firebase/database";
+
+type Props = {
+  data: IComedian[];
+  title?: string;
+};
+
+function ComedianGrid({ data, title = "Recently Added Comedians" }: Props) {
+  return (
+    data && (
+      <section className="comedians">
+        <h3 className="comedians__header">{title}</h3>
+        <div className="comedians__grid">
+          {data.map((comedian: IComedian) => (
+            <ComedianCard {...comedian} key={comedian.id} />
+          ))}
+        </div>
+      </section>
+    )
+  );
+}
+
+export default ComedianGrid;
