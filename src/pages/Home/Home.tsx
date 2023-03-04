cf2adfb7c57dda63e0acfbb5bf9d67e76519920f
Bryan Miller <bmilcs@yahoo.com>
2023-03-04 16:51:49 -0500

    Refactor home: add topFavorites & consistent sorting

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,54 +2,108 @@ import ComedianGrid from '@/components/ComedianGrid/ComedianGrid';
 import SpecialsGrid from '@/components/SpecialsGrid/SpecialsGrid';
 import {
   IComedian,
-  IComedySpecial,
+  ISpecial,
   getLatestComediansFromDB,
   getLatestSpecialsFromDB,
+  getTopFavoriteComediansFromDB,
+  getTopFavoriteSpecialsFromDB,
   getUpcomingSpecialsFromDB,
 } from '@/firebase/database';
+import { isDateOneBeforeDateTwo } from '@/utils/date';
 import { useEffect, useState } from 'react';
 import './Home.scss';
 
 function Home() {
-  const [latestSpecials, setLatestSpecials] = useState<IComedySpecial[]>([]);
+  const [latestSpecials, setLatestSpecials] = useState<ISpecial[]>([]);
+  const [upcomingSpecials, setUpcomingSpecials] = useState<ISpecial[]>([]);
+  const [topFavoriteSpecials, setTopFavoriteSpecials] = useState<ISpecial[]>([]);
   const [latestComedians, setLatestComedians] = useState<IComedian[]>([]);
-  const [upcomingSpecials, setUpcomingSpecials] = useState<IComedySpecial[]>([]);
+  const [topFavoriteComedians, setTopFavoriteComedians] = useState<IComedian[]>([]);
 
-  // on first page load, retrieve latest/upcoming specials & comedians
+  // on first page load...
   useEffect(() => {
     const getLatestSpecials = async () => {
       const latestData = await getLatestSpecialsFromDB();
       if (!latestData) return;
-
-      const latest: IComedySpecial[] = [];
-      for (const special in latestData) {
-        latest.push(latestData[special]);
-      }
-      setLatestSpecials(latest);
+      const sortedLatest = Object.keys(latestData)
+        .map((string) => Number(string))
+        .sort((a, b) => {
+          const aDate = latestData[a].release_date;
+          const bDate = latestData[b].release_date;
+          return isDateOneBeforeDateTwo(aDate, bDate) ? 1 : -1;
+        })
+        .map((specialId) => {
+          return latestData[specialId];
+        });
+      setLatestSpecials(sortedLatest);
     };
 
     const getUpcomingSpecials = async () => {
       const upcomingData = await getUpcomingSpecialsFromDB();
       if (!upcomingData) return;
-
-      const upcoming: IComedySpecial[] = [];
-      for (const special in upcomingData) {
-        upcoming.push(upcomingData[special]);
-      }
-      setUpcomingSpecials(upcoming);
+      const sortedUpcoming = Object.keys(upcomingData)
+        .map((string) => Number(string))
+        .sort((a, b) => {
+          const aDate = upcomingData[a].release_date;
+          const bDate = upcomingData[b].release_date;
+          return isDateOneBeforeDateTwo(aDate, bDate) ? 1 : -1;
+        })
+        .map((specialId) => {
+          return upcomingData[specialId];
+        });
+      setUpcomingSpecials(sortedUpcoming);
     };
 
     const getLatestComedians = async () => {
       const latestComedians = await getLatestComediansFromDB();
       if (!latestComedians) return;
-
-      const latest = [];
-      for (const comedian in latestComedians) {
-        latest.push(latestComedians[comedian]);
-      }
-      setLatestComedians(latest);
+      const sortedLatest = Object.keys(latestComedians)
+        .map((string) => Number(string))
+        .sort((a, b) => {
+          const aDate = latestComedians[a].dateAdded.seconds;
+          const bDate = latestComedians[b].dateAdded.seconds;
+          return aDate > bDate ? -1 : 1;
+        })
+        .map((specialId) => {
+          return latestComedians[specialId];
+        });
+      setLatestComedians(sortedLatest);
     };
 
+    const getTopFavoriteComedians = async () => {
+      const comedians = await getTopFavoriteComediansFromDB();
+      if (!comedians) return;
+      const topFavoriteComedians = Object.keys(comedians)
+        .map((string) => Number(string))
+        .sort((a, b) => {
+          const aFavorites = comedians[a].favorites;
+          const bFavorites = comedians[b].favorites;
+          return aFavorites > bFavorites ? -1 : 1;
+        })
+        .map((specialId) => {
+          return comedians[specialId];
+        });
+      setTopFavoriteComedians(topFavoriteComedians);
+    };
+
+    const getTopFavoriteSpecials = async () => {
+      const specials = await getTopFavoriteSpecialsFromDB();
+      if (!specials) return;
+      const topFavoriteSpecials = Object.keys(specials)
+        .map((string) => Number(string))
+        .sort((a, b) => {
+          const aFavorites = specials[a].favorites;
+          const bFavorites = specials[b].favorites;
+          return aFavorites > bFavorites ? -1 : 1;
+        })
+        .map((specialId) => {
+          return specials[specialId];
+        });
+      setTopFavoriteSpecials(topFavoriteSpecials);
+    };
+
+    getTopFavoriteComedians();
+    getTopFavoriteSpecials();
     getLatestComedians();
     getLatestSpecials();
     getUpcomingSpecials();
@@ -57,11 +111,25 @@ function Home() {
 
   return (
     <div className='column'>
-      {latestSpecials && <SpecialsGrid title='Latest Releases' data={latestSpecials} />}
+      {latestSpecials && latestSpecials.length !== 0 && (
+        <SpecialsGrid title='Latest Releases' data={latestSpecials} />
+      )}
 
-      {upcomingSpecials && <SpecialsGrid title='Coming Soon' data={upcomingSpecials} />}
+      {topFavoriteComedians && topFavoriteComedians.length !== 0 && (
+        <ComedianGrid title='Most Popular Comedians' data={topFavoriteComedians} />
+      )}
 
-      {latestComedians && <ComedianGrid data={latestComedians} title='Recently Added Comedians' />}
+      {upcomingSpecials && upcomingSpecials.length !== 0 && (
+        <SpecialsGrid title='Coming Soon' data={upcomingSpecials} />
+      )}
+
+      {topFavoriteSpecials && topFavoriteSpecials.length !== 0 && (
+        <SpecialsGrid title='Most Popular Specials' data={topFavoriteSpecials} />
+      )}
+
+      {latestComedians && latestComedians.length !== 0 && (
+        <ComedianGrid title='Recently Added Comedians' data={latestComedians} />
+      )}
     </div>
   );
 }
