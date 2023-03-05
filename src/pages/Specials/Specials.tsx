f565c77926c64f425b1ea22733b77639b3b345a5
Bryan Miller <bmilcs@yahoo.com>
2023-03-05 18:00:25 -0500

    Add basic all specials page functionality

--- src/pages/Specials/Specials.tsx
+++ src/pages/Specials/Specials.tsx
@@ -1,9 +1,29 @@
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import SpecialsGrid from '@/components/SpecialsGrid/SpecialsGrid';
+import { allSpecialsDataArr, fetchAllSpecials } from '@/features/allSpecialsSlice/allSpecialsSlice';
+import { ISpecial } from '@/firebase/database';
+import { useEffect, useState } from 'react';
 import './Specials.scss';
 
 function Specials() {
+  const dispatch = useAppDispatch();
+  const allSpecials = useAppSelector(allSpecialsDataArr);
+  const [sortedSpecials, setSortedSpecials] = useState<ISpecial[]>([]);
+
+  useEffect(() => {
+    if (!allSpecials || allSpecials.length === 0) dispatch(fetchAllSpecials());
+  }, []);
+
+  useEffect(() => {
+    if (allSpecials.length === 0) return;
+    const specialsCopy = [...allSpecials];
+    specialsCopy.sort((a, b) => (a.title > b.title ? 1 : -1));
+    setSortedSpecials(specialsCopy);
+  }, [allSpecials]);
+
   return (
     <div className='column'>
-      <h2>Specials</h2>
+      {sortedSpecials && sortedSpecials.length !== 0 && <SpecialsGrid data={sortedSpecials} />}
     </div>
   );
 }
