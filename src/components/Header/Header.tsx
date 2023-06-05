441cd29f11791583d375acfd2841144f273db84e
Bryan Miller <bmilcs@yahoo.com>
2023-06-05 16:14:23 -0400

    Reduce header animation time

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,7 +26,7 @@ function Header() {
       className='header'
       initial={{ opacity: 0, y: -60 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.75, type: 'spring', stiffness: 30, mass: 0.2 }}
+      transition={{ duration: 0.25, type: 'spring', stiffness: 50, mass: 0.2 }}
     >
       <div className='column'>
         <Link to='/'>
